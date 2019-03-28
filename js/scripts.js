1b2d07e3d4d8ffafef329699ede754eb25c81af4
Dominic Montelongo and Reese Lee <dominic@>
2019-03-28 11:57:32 -0700

    add CSS to images, initial js, clean up index.html

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+// var animal = $("input:radio[name='animal']:checked").val();
+// var animal = $("input:radio[name=animal]:checked").val();
+
+$(document).ready(function() {
+  $("input[type='button']").click(function(){
+    if($('input:checked #Seals')) {
+      $("Seals").show();
+    }
+
+
+
+
+  });
+
+});
+//
+// $(document).ready(function() {
+//   $("value#Seals").click(function() {
+//     $("#Seals").show();
+//   });
+//
+// });
